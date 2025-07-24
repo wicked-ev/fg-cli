87c628dfe7589e5710451efa14a49a4056005193
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-07-25 00:20:15 +0100

    Refactor createComp function to improve component setup flow and enhance directory creation prompts

--- src/commands/createComp.js
+++ src/commands/createComp.js
@@ -1,133 +1,185 @@
-import path from 'path'
-import fs from 'fs';
-import { select } from '@clack/prompts';
+import path from "path";
+import fs from "fs";
+import { select } from "@clack/prompts";
 
-
-export async function createComp (name, force = false, compType = 'jsx', ignoreDefault) {
-    
-    const compName = name[0].toUpperCase() + name.slice(1);
-    let compContent = `import './styles/${compName}.css';
+export async function createComp(
+  name,
+  force = false,
+  compType = "jsx",
+  ignoreDefault
+) {
+  const compName = name[0].toUpperCase() + name.slice(1);
+  let compContent = `import './styles/${compName}.css';
 
 export default function ${compName}() {
 
     return (<></>);
-}`; 
+}`;
 
-    const currentPath = process.cwd();
+  const currentPath = process.cwd();
 
-    if (!ignoreDefault) {
-        setupDefaultComponent(currentPath,compName,compType,compContent,force);
+  if (!ignoreDefault) {
+    await setupDefaultComponent(
+      currentPath,
+      compName,
+      compType,
+      compContent,
+      force
+    );
+  } else {
+    await setupCustomComponent(
+      currentPath,
+      compName,
+      compType,
+      compContent,
+      force
+    );
+  }
+}
 
-    } else {
-        setupCustomComponent(currentPath,compName,compType,compContent,force);
+async function setupCustomComponent(
+  currentPath,
+  compName,
+  compType,
+  compContent,
+  force
+) {
+  const srcDir = await select({
+    message: "create src directory",
+    options: [
+      { value: false, label: "no" },
+      { value: true, label: "yes" },
+    ],
+  });
+  const compDir = await select({
+    message: "create components directory",
+    options: [
+      { value: false, label: "no" },
+      { value: true, label: "yes" },
+    ],
+  });
+
+  const stylesDir = await select({
+    message: "create styles directory",
+    options: [
+      { value: false, label: "no" },
+      { value: true, label: "yes" },
+    ],
+  });
+
+  let compDirPath = [currentPath];
+  let newCompPath = path.join(currentPath, `${compName}.${compType}`);
+  let newCompStyle = path.join(currentPath, `${compName}.css`);
+
+  if (!compDir && !stylesDir) {
+    const writeRoot = await select({
+      message: "files are being create in root directory are you sure?",
+      options: [
+        { value: false, label: "no" },
+        { value: true, label: "yes" },
+      ],
+    });
+    if (!writeRoot) {
+      console.log("operation Cancelled");
+      process.exit(0);
     }
-} 
+  }
+  if (srcDir) {
+    compDirPath.push("src");
+  }
+  if (compDir) {
+    compDirPath.push("components");
+    newCompPath = path.join(...compDirPath, `${compName}.${compType}`);
+  }
 
-
-async function setupCustomComponent(currentPath,compName,compType,compContent,force) {
-      const srcDir = await select({
-            message: 'create src directory', 
-            options: [
-                {value:false, label:'no'},
-                {value:true, label:'yes'},
-            ], 
-        });
-        const compDir = await select({
-            message: 'create components directory', 
-            options: [
-                {value:false, label:'no'},
-                {value:true, label:'yes'},
-            ], 
-        });
-        
-        const stylesDir = await select({
-            message: 'create styles directory', 
-            options: [
-                {value:false, label:'no'},
-                {value:true, label:'yes'},
-            ], 
-        });
-        
-        if(!compDir && !stylesDir) {
-           const writeRoot = await select({
-            message: 'files are being create in root directory are you sure?', 
-            options: [
-                {value:false, label:'no'},
-                {value:true, label:'yes'},
-            ], 
-        }); 
-            if (!writeRoot) {
-            console.log("operation Cancelled");
-            process.exit(0);
-            }
-        }
-        let compDirPath = [currentPath];
-        let newCompPath = currentPath;
-        let newCompStyle = currentPath;
-        if (srcDir) {
-            compDirPath.push('src');
-        }
-        if (compDir) {
-            compDirPath.push('components');
-            newCompPath = path.join(...compDirPath, `${compName}.${compType}`);
-        }
-
-        if (stylesDir) {
-            compDirPath.push('styles');
-            newCompStyle = path.join(...compDirPath, `${compName}.css`);
-        } else {
-            compContent = `import './${compName}.css';
+  if (stylesDir) {
+    compDirPath.push("styles");
+    newCompStyle = path.join(...compDirPath, `${compName}.css`);
+  } else {
+    compContent = `import './${compName}.css';
 
 export default function ${compName}() {
 
     return (<></>);
-}`
-        }
-        
-        compDirPath = path.join(...compDirPath);
-        
-        createCompFiles(compDirPath,newCompPath,newCompStyle,compName,compType,compContent,force);
+}`;
+  }
+  console.log("compDirPath:" + compDirPath);
+  console.log("compStyle:" + newCompStyle);
+  console.log("compPath:" + newCompPath);
+  compDirPath = path.join(...compDirPath);
+
+  createCompFiles(
+    compDirPath,
+    newCompPath,
+    newCompStyle,
+    compName,
+    compType,
+    compContent,
+    force
+  );
 }
-async function setupDefaultComponent(currentPath,compName,compType,compContent,force){
-        
-        const componentDir = path.join(currentPath, 'src', 'components');
-        const stylesDir = path.join(componentDir, 'styles');
-        const newCompPath = path.join(componentDir, `${compName}.${compType}`);
-        const newCompStyle = path.join(stylesDir, `${compName}.css`);
+async function setupDefaultComponent(
+  currentPath,
+  compName,
+  compType,
+  compContent,
+  force
+) {
+  const componentDir = path.join(currentPath, "src", "components");
+  const stylesDir = path.join(componentDir, "styles");
+  const newCompPath = path.join(componentDir, `${compName}.${compType}`);
+  const newCompStyle = path.join(stylesDir, `${compName}.css`);
 
-        createCompFiles(stylesDir,newCompPath,newCompStyle,compName,compType,compContent,force);
+  createCompFiles(
+    stylesDir,
+    newCompPath,
+    newCompStyle,
+    compName,
+    compType,
+    compContent,
+    force
+  );
 }
 
-function createCompFiles(dirPath, newCompPath, newCompStyle,compName,compType,compContent
-    ,force) {
-        try {
-            fs.mkdirSync(dirPath, {recursive: true});
-        } catch (error) {
-            console.error('❌ Failed to create directory:', error.message);
-            process.exit(1);
-        }
+function createCompFiles(
+  dirPath,
+  newCompPath,
+  newCompStyle,
+  compName,
+  compType,
+  compContent,
+  force
+) {
+  try {
+    fs.mkdirSync(dirPath, { recursive: true });
+  } catch (error) {
+    console.error("❌ Failed to create directory:", error.message);
+    process.exit(1);
+  }
 
-        try {
-            const fileExists = fs.existsSync(newCompPath);
-            const cssExists = fs.existsSync(newCompStyle);
-    
-            if (!fileExists || force) {
-                fs.writeFileSync(newCompPath, compContent);
-                console.log(`✅ ${fileExists ? 'Overwritten' : 'Created' } ${compName}`);
-            } else {
-                console.log(`⚠️  Component ${compName}.${compType} already exists! you can use --force or -f to overwrite`);
-            }
-            
-            if(!cssExists || force) {
-                fs.writeFileSync(newCompStyle, '');
-                console.log(`✅ ${cssExists ? 'Overwritten' : 'Created' } ${compName}`);
-            } else {
-                console.log(`⚠️  Style ${compName}.css already exists!  you can use --force or -f to overwrite`);
-            }
-            
-        } catch (error) {
-            console.error('❌ Failed to write file:', error.message);
-            process.exit(1);
-        }
+  try {
+    const fileExists = fs.existsSync(newCompPath);
+    const cssExists = fs.existsSync(newCompStyle);
 
+    if (!fileExists || force) {
+      fs.writeFileSync(newCompPath, compContent);
+      console.log(`✅ ${fileExists ? "Overwritten" : "Created"} ${compName}`);
+    } else {
+      console.log(
+        `⚠️  Component ${compName}.${compType} already exists! you can use --force or -f to overwrite`
+      );
+    }
+
+    if (!cssExists || force) {
+      fs.writeFileSync(newCompStyle, "");
+      console.log(`✅ ${cssExists ? "Overwritten" : "Created"} ${compName}`);
+    } else {
+      console.log(
+        `⚠️  Style ${compName}.css already exists!  you can use --force or -f to overwrite`
+      );
+    }
+  } catch (error) {
+    console.error("❌ Failed to write file:", error.message);
+    process.exit(1);
+  }
 }
