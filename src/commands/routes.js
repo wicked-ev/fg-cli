8743a041196ac14a3603b825f2e080c0cb392b20
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-07-29 01:28:21 +0100

    refactored addRoute, scanForRouters, isVaildRouter function, added insertRouteJSXElement defintion

--- src/commands/routes.js
+++ src/commands/routes.js
@@ -5,20 +5,21 @@ import path from "path";
 import chalk from "chalk";
 import bableParser from "@babel/parser";
 import * as recast from "recast";
-export async function addRoute(routerName) {
-  // fg route routeName routeComp
-  //     - check if comp exists
-  //         - if yes we have to import to the route export
-  //             - i need to scan the directory for jsx file
-  //                 - with exception for some directories (node modules, testing etc);
-  //                 - or i can just list all the comps to select one
-  //         - if no we have to let him know before adding that route
+export async function addRoute(routerName, routePath, component) {
+  // fg route routeName routeCompe
 
   const currentPath = process.cwd();
-
   if (routerName) {
     const router = path.join(currentPath, `${routerName}.jsx`);
     if (fs.existsSync(router)) {
+      if (isValidRouter(filePath)) {
+        //insert new route
+      } else {
+        console.log(
+          chalk.red(`jsx file named ${routerName} is not valid router`),
+        );
+        process.exit(0);
+      }
     } else {
       const confirmRouterSearch = await confirm({
         message: `No .jsx file named ${routerName} in This directory, would you like to search for vaible router in this directory`,
@@ -29,30 +30,23 @@ export async function addRoute(routerName) {
       }
     }
   }
-  const files = await fg([`**/*.jsx`, "!node_modules/**", "!.git/**"], {
-    dot: true,
-  });
 
-  const options = [{ value: null, label: "custom file name" }];
+  const routers = scanForRouters();
 
-  if (files.length >= 1) {
-    for (const file of files) {
-      options.push({ value: file, label: path.basename(file) });
+  if (routers) {
+    for (const router of routers) {
+      options.push({ value: router, label: path.basename(router) });
     }
-    const router = await autocomplete({
+    const selectedRouter = await autocomplete({
       message: "Select Components",
       options: options,
       placeholder: "Type to search...",
       maxItems: 5,
     });
+    //insert
   } else {
-    const makeCustom = await confirm({
-      message: "No .jsx files found. Do you want to specify a custom name?",
-    });
-    if (!makeCustom) {
-      console.log(chalk.red("Operation canceled"));
-      process.exit(1);
-    }
+    console.log(chalk.red("No viable router files found"));
+    process.exit(1);
   }
 }
 
@@ -61,12 +55,10 @@ async function scanForRouters() {
     dot: true,
   });
 
-  if (!files) return files;
-
   const routers = [];
   for (const file of files) {
     if (isValidRouter(file)) {
-      routers.push(files);
+      routers.push(file);
     }
   }
   return routers;
@@ -80,8 +72,32 @@ function readFileContent(filePath) {
     console.error(`error reading ${filePath}: ${error.message}`);
   }
 }
+
 function isValidRouter(filePath) {
   const sourceCode = readFileContent(filePath);
+  const isRouter = false;
+
+  const ast = getAST(sourceCode);
+
+  recast.types.visit(ast, {
+    visitJSXElement(path) {
+      const openEl = path.node.openingElement;
+      const tagName = path.openingElement.name.name;
+      const n = recast.types.namedTypes;
+      if (
+        tagName == "Routes" ||
+        tagName == "BrowserRouter" ||
+        tagName == "Route"
+      ) {
+        isRouter = true;
+      }
+    },
+  });
+
+  return isRouter;
+}
+
+function getAST(sourceCode) {
   const ast = recast.parse(sourceCode, {
     parser: {
       parse(code) {
@@ -92,25 +108,19 @@ function isValidRouter(filePath) {
       },
     },
   });
-
-  const builder = recast.types.builders;
-  recast.types.visit(ast, {
-    visitJSXElement(path) {
-      const openEl = path.node.openingElement;
-      const tagName = path.openingElement.name.name;
-      const n = recast.types.namedTypes;
-      if (tagName == "Route") {
-        const attributes = openEl.attributes;
-        for (const attribute of attributes) {
-          if (
-            attribute.name.name === "path" &&
-            n.JSXAttribute.check(attribute) &&
-            attribute.value.type === "Literal" &&
-            attribute.value.value === "/"
-          ) {
-          }
-        }
-      }
-    },
-  });
+  return ast;
 }
+
+function insertRoutJSXElement(router, routePath, component) {
+  //first look for route with path / if not
+  // look for Routes Element to insert if not
+  // look for BrowserRouter Element and create Routes Element inside it then insert your route
+}
+
+// const route = builder.jsxElement(
+//   builder.jsxOpeningElement(builder.jsxIdentifier("Route")),
+//   [
+//     builder.jsxAttribute(builder.jsxIdentifier("path"), builder.literal()),
+//     builder.jsxAttribute(builder.jsxIdentifier("comp")),
+//   ],
+// );
