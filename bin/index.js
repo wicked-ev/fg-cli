8b374f4a1ed41c0101bc3eedb92054e2eede56b0
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-07-31 01:07:03 +0100

    refactor: streamline route addition logic in main function

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 import arg from "arg";
 import { createComp } from "../src/commands/createComp.js";
 import { listComps } from "../src/commands/list.js";
+import { addRoute } from "../src/commands/routes.js";
 
 async function main() {
   const args = arg({
@@ -22,23 +23,28 @@ async function main() {
   if (args["--create"]) {
     const compType = args["-t"] ? "tsx" : "jsx";
     createComp(args["--create"], args["--force"], compType, args["--ignore"]);
-
-    
   }
   if (args["--list"]) {
     await listComps(args["--ext"]);
   }
+
+  if (args["--radd"] && args["_"].length > 1) {
+    console.log(args);
+    const routePath = args["_"][0];
+    let component = args["_"][1];
+    component = component[0].toUpperCase() + component.slice(1);
+    addRoute(args["--radd"], routePath, component);
+  }
   /*
     fg -c Task
     fg route routeName routeComp
-        - check if comp exists 
+        - check if comp exists
             - if yes we have to import to the route export
-                - i need to scan the directory for jsx file
-                    - with exception for some directories (node modules, testing etc);
+                - i need to scan the directory for jsx
                     - or i can just list all the comps to select one
             - if no we have to let him know before adding that route
-    fg list both jsx tsx 
-        - fileJSX -> path      
+    fg list both jsx tsx
+        - fileJSX -> path
 */
 }
 
