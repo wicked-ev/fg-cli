2d74c0b2099a022c0938325a1129fe32ddc6fc7f
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-08-08 19:37:24 +0100

    refactor: update command definitions for improved clarity and functionality

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-import arg from "arg";
 import { Command } from "commander";
 import { createComp } from "../src/commands/createComp.js";
 import { listComps } from "../src/commands/list.js";
 import { addRoute } from "../src/commands/routes.js";
-import playTest from "../utils/playTest.js";
 
 
-//todo: maybe maaaaaybe change the commands here to use commander for better commands handling
+//done: maybe maaaaaybe change the commands here to use commander for better commands handling
 //todo: polish add route better then consider releasing? 
 //todo: help command
 //todo: command that can look for JSX Element and changes its Identifier
@@ -20,67 +18,32 @@ async function main() {
       .version("0.0.1");
 
     program.
-      command("cr")
-      .description("create react component")
-      .option("-t","creates typescript component .tsx")
+      command("cr <compName>")
+      .description("creates react component")
+      .option("-t, --typescript","creates typescript component .tsx")
       .option("-f, --force", "force to overwrite if component with same name already exists")
-      .option("-i, --ignore", "allows for custom creation of the component");
+      .option("-i, --ignore", "allows for custom creation of the component")
+      .action((compName, option) => {
+        const type = option.typescript ? "tsx" : "jsx";
+        createComp(compName, option.force,type, option.ignore);
+      });
 
     program.
-      command("arr")
+      command("arr <router> <path> <component>")
         .description("adds route to react router if found")
-    
+        .action((router, path, component) => {
+          addRoute(router, path, component);
+        });
+  
     program.
       command("list")
-        .description("list files based on extensions (default is .jsx")
-        .option("-e, --ext", "specifies extension type")
-        
-  //fg cr compName -i 
-  // const args = arg({
-  //   "--create": String,
-  //   "--force": Boolean,
-  //   "--list": Boolean,
-  //   "--ext": String,
-  //   "--radd": String,
-  //   "--rset": String,
-  //   "--playtest": Boolean,
-  //   "-l": "--list",
-  //   "-c": "--create",
-  //   "-t": Boolean,
-  //   "-f": "--force",
-  //   "--ignore": Boolean,
-  //   "-i": "--ignore",
-  // });
+        .description("list files based on extensions (default is .jsx)")
+        .option("-e, --ext <ext>", "specifies extension type")
+        .action((option) => {
+          listComps(option.ext);
+        });
 
-  // if (args["--create"]) {
-  //   const compType = args["-t"] ? "tsx" : "jsx";
-  //   createComp(args["--create"], args["--force"], compType, args["--ignore"]);
-  // }
-  // if (args["--list"]) {
-  //   await listComps(args["--ext"]);
-  // }
-
-  // if (args["--radd"] && args["_"].length > 1) {
-  //   console.log(args);
-  //   const routePath = args["_"][0];
-  //   let component = args["_"][1];
-  //   component = component[0].toUpperCase() + component.slice(1);
-  //   addRoute(args["--radd"], routePath, component);
-  // }
-  // if (args["--playtest"]) {
-  //   playTest();
-  // }
-  /*
-    fg -c Task
-    fg route routeName routeComp
-        - check if comp exists
-            - if yes we have to import to the route export
-                - i need to scan the directory for jsx
-                    - or i can just list all the comps to select one
-            - if no we have to let him know before adding that route
-    fg list both jsx tsx
-        - fileJSX -> path
-*/
+    program.parse();
 }
 
 main();
