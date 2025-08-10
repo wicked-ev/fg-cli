9fcfb4585bd8b25e252c84932ef91fd18b8e5a5b
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-08-11 00:40:54 +0100

    refactor: update command structure and descriptions for improved clarity and functionality, add new command for creating hooks (WIP)

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { createComp } from "../src/commands/createComp.js";
-import { listComps } from "../src/commands/list.js";
+import { createHook } from "../src/commands/createHook.js";
+import { listComps } from "../src/commands/listFiles.js";
 import { addRoute } from "../src/commands/routes.js";
 
-
-//done: maybe maaaaaybe change the commands here to use commander for better commands handling
-//todo: polish add route better then consider releasing? 
-//todo: help command
+//todo: create hooks 
 //todo: command that can look for JSX Element and changes its Identifier
 
 async function main() {
@@ -19,15 +17,29 @@ async function main() {
 
     program.
       command("cr <compName>")
-      .description("creates react component")
+      .description("creates react component template")
       .option("-t, --typescript","creates typescript component .tsx")
       .option("-f, --force", "force to overwrite if component with same name already exists")
       .option("-i, --ignore", "allows for custom creation of the component")
-      .action((compName, option) => {
-        const type = option.typescript ? "tsx" : "jsx";
-        createComp(compName, option.force,type, option.ignore);
+      .option("-s, --style <style>", "CSS style type: css | scss | module (default: css)")
+      .action((compName, options) => {
+        const styleMap = 
+        {
+          scss: "scss",
+          css: "css",
+          module: "module.css"
+        }
+        const cssType = styleMap[options.style] || "css";
+        const type = options.typescript ? "tsx" : "jsx";
+        createComp(compName, options.force, type, cssType, options.ignore);
       });
-
+      
+      program.command("hr <hookName>")
+          .description("creates custom react hook template")
+          .option("-i, --ignore", "ignores to check for hooks directory")
+          .action((hookName, options) => {
+            createHook(hookName,options.ignore);
+          })
     program.
       command("arr <router> <path> <component>")
         .description("adds route to react router if found")
@@ -43,6 +55,7 @@ async function main() {
           listComps(option.ext);
         });
 
+    // fg hr <name>
     program.parse();
 }
 
